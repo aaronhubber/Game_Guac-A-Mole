eb09e890380ecfa5e72e87f007423eb3b83ac886
Aaron Hubber <aaronhubber@gmail.com>
2022-05-20 16:29:59 +0100

    code ready, edits to be made

---
+++ src/components/Mole.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+
+
+export const MOLE_SCORE = 100
+export const POINTS_MULTIPLIER = 0.9
+export const TIME_MULTIPLYER = 1.5
+
+export const Mole = ({ onWhack, points, delay, speed, pointsMin = 10 }) => {
+    const [hit, setHit] = useState(false)
+    const bobRef = useRef(null)
+    const pointsRef = useRef(points)
+    const buttonRef = useRef(null)
+    useEffect(() => {
+        gsap.set(buttonRef.current, {
+            yPercent: 100,
+            display: 'block'
+        })
+            bobRef.current = gsap.to(buttonRef.current, {
+            yPercent: 0,
+            duration: speed,
+            yoyo: true,
+            repeat: -8,
+            delay: delay,
+            repeatDelay: delay,
+        onRepeat: () => {
+            pointsRef.current = Math.floor(
+            Math.max(pointsRef.current * POINTS_MULTIPLIER, pointsMin)
+        )
+        },
+        })
+        return () => {
+            if (bobRef.current) bobRef.current.kill()
+    }
+    }, [pointsMin, delay, speed])
+
+    useEffect(()=>{
+        if (hit) {
+            pointsRef.current = points
+            bobRef.current.pause()
+            gsap.to(buttonRef.current, {
+                yPercent: 100,
+                duration: 0.1,
+                onComplete:() =>{
+                    gsap.delayedCall(gsap.utils.random(1,3),()=>{
+                        setHit(false)
+                        bobRef.current.restart()
+                    .timeScale(bobRef.current.timeScale()* TIME_MULTIPLYER)
+                    })
+                },
+            })
+        }
+    }, [hit])
+
+    const whack = ()=>{
+        setHit(true)
+        onWhack(pointsRef.current)
+    }
+        return (
+            <div className="mole-hole">
+            <button
+                className="mole"
+                ref={buttonRef}
+                onClick={whack}
+            >Mole
+            </button>
+            </div>
+        )
+    }
+
+    
+    
+    export const Score = ({ value }) => <div>{`Score: ${value}`}</div>
---
+++ src/containers/GameContainer.js
@@ -0,0 +1,70 @@
+import React, {useState} from "react";
+import { render } from "@testing-library/react";
+import Timer from "../components/Timer";
+import { Mole, MOLE_SCORE } from "../components/Mole";
+import { Score } from "../components/Mole";
+
+
+const TIME_LIMIT = 30000
+
+
+
+const Moles = ({children}) => {
+    return <div>{children}</div>
+}
+
+
+const Game = (()=>{
+    const [playing, setPlaying] = useState(false)
+    const [finished, setFinished] = useState(false)
+    const [score, setScore] = useState(0)
+
+
+    const onWhack = points => setScore(score + points)
+    
+
+    const endGame = (()=>{
+        setPlaying (false)
+        setFinished(true)
+    })
+
+    const startGame = (()=>{
+        setScore (0)
+        setPlaying (true)
+        setFinished(false)
+    })
+    return(
+        <>
+            {!playing && 
+            !finished && 
+        <>
+            <h1>Whac-a-Mole</h1> 
+            <button onClick={startGame}>Start Game</button>
+            </>}
+            {playing && 
+        <>
+            <button onClick={endGame}>End Game</button>
+            <Score value={score}/>
+            <Timer time={TIME_LIMIT} onEnd={endGame}/>
+            <Moles>
+                <Mole onWhack={onWhack} points={MOLE_SCORE} delay={0} speed={2}/>
+                <Mole onWhack={onWhack} points={MOLE_SCORE} delay={0} speed={2}/>
+                <Mole onWhack={onWhack} points={MOLE_SCORE} delay={0} speed={2}/>
+                <Mole onWhack={onWhack} points={MOLE_SCORE} delay={0} speed={2}/>
+                <Mole onWhack={onWhack} points={MOLE_SCORE} delay={0} speed={2}/>
+                <Mole onWhack={onWhack} points={MOLE_SCORE} delay={0} speed={2}/>
+
+            </Moles>
+        </>
+            }
+            { finished &&
+        <>
+            <Score value= {score}/>
+            <button onClick={startGame}>Play Again</button>
+        </>
+            }
+        </>
+    )
+})
+
+export default Game
